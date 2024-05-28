d1b9cc47ebd7d048b3b50207327f001b6702af6c
Mechanist <cooksleymelody@gmail.com>
2024-05-28 11:36:47 -0500

    automatic week, month, year updates

--- budget/management/commands/update_timeperiods.py
+++ budget/management/commands/update_timeperiods.py
@@ -3,7 +3,32 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from budget.models import TimePeriod, Category, CurrentTimePeriod
 from datetime import timedelta
-from calendar import monthrange
+
+
+def latest_time_period_by_type(user, type):
+    return TimePeriod.objects.filter(user=user, type=type).order_by('-ordering').first()
+
+
+def time_period_exists(user, period_type):
+    return TimePeriod.objects.filter(user=user, type=period_type).exists()
+
+
+def copy_categories(user, prev, new):
+    categories = Category.objects.filter(user=user, timeperiod=prev)
+    for category in categories:
+        Category.objects.create(
+            user=user,
+            timeperiod=new,
+            category=category.category,
+            amount=category.amount
+        )
+
+
+def is_in_current_period(prev, fut):
+    now = int(timezone.now().timestamp())
+    print(f'{prev} < {now} < {fut}')
+    return prev <= now <= fut
+
 
 class Command(BaseCommand):
     help = 'Update time periods and create new categories'
@@ -14,63 +39,60 @@ class Command(BaseCommand):
         users = User.objects.all()
 
         for user in users:
-            # Init time periods for user
-            self.initialize_time_periods(now, user)
-
             # Get most up-to-date time period
-            latest_time_period = TimePeriod.objects.filter(user=user).order_by('-index').first()
+            latest_week = latest_time_period_by_type(user, "week")
+            latest_month = latest_time_period_by_type(user, "month")
+            latest_year = latest_time_period_by_type(user, "year")
 
-            # TODO: Make this work with day, month, and year
-            # TODO: Update check if latest is not current
-            if latest_time_period:
-                new_index = latest_time_period.index + 1
-
-                if latest_time_period.type == 'day':
-                    new_index = latest_time_period.index + timedelta(days=1).total_seconds()
-                elif latest_time_period.type == 'week':
-                    new_index = latest_time_period.index + timedelta(weeks=1).total_seconds()
-                elif latest_time_period.type == 'month':
-                    next_month = (timezone.datetime.fromtimestamp(latest_time_period.index).replace(day=28) + timedelta(days=4)).replace(day=1)
-                    new_index = int(next_month.timestamp())
-                elif latest_time_period.type == 'year':
-                    next_year = timezone.datetime.fromtimestamp(latest_time_period.index).replace(year=timezone.datetime.fromtimestamp(latest_time_period.index).year + 1, month=1, day=1)
-                    new_index = int(next_year.timestamp())
-                else:
-                    continue
-
-                new_time_period = TimePeriod.objects.create(
-                    user=user,
-                    type=latest_time_period.type,
-                    index=int(new_index)
-                )
-
-                # Copy categories from the previous time period
-                categories = Category.objects.filter(user=user, timeperiod=latest_time_period)
-                for category in categories:
-                    Category.objects.create(
+            if not latest_week and latest_month and latest_year:
+                # Week
+                new_index_week = latest_week.index + timedelta(weeks=1).total_seconds()
+                if is_in_current_period(latest_week.index, new_index_week) and not latest_week:
+                    new_time_period_week = TimePeriod.objects.create(
                         user=user,
-                        timeperiod=new_time_period,
-                        category=category.category,
-                        amount=category.amount
+                        type="week",
+                        index=int(new_index_week)
                     )
+                    copy_categories(user, latest_week, new_time_period_week)
+                    self.stdout.write(self.style.SUCCESS(f'Created new time period {new_time_period_week} for user {user}'))
 
-                self.stdout.write(self.style.SUCCESS(f'Created new time period {new_time_period} for user {user}'))
+                # Month
+                next_month = (timezone.datetime.fromtimestamp(latest_month.index).replace(day=28) + timedelta(days=4)).replace(day=1)
+                new_index_month = int(next_month.timestamp())
+                if is_in_current_period(latest_month.index, new_index_month) and not latest_month:
+                    new_time_period_month = TimePeriod.objects.create(
+                        user=user,
+                        type="month",
+                        index=int(new_index_month)
+                    )
+                    copy_categories(user, latest_month, new_time_period_month)
+                    self.stdout.write(self.style.SUCCESS(f'Created new time period {new_time_period_month} for user {user}'))
+
+                # Year
+                next_year = timezone.datetime.fromtimestamp(latest_year.index).replace(year=timezone.datetime.fromtimestamp(latest_year.index).year + 1, month=1, day=1)
+                new_index_year = int(next_year.timestamp())
+                if is_in_current_period(latest_year.index, new_index_year) and not latest_year:
+                    new_time_period_year = TimePeriod.objects.create(
+                        user=user,
+                        type="year",
+                        index=int(new_index_year)
+                    )
+                    copy_categories(user, latest_year, new_time_period_year)
+                    self.stdout.write(self.style.SUCCESS(f'Created new time period {new_index_year} for user {user}'))
             else:
                 self.initialize_time_periods(now, user)
-    def time_period_exists(self, user, period_type):
-        return TimePeriod.objects.filter(user=user, type=period_type).exists()
 
     def initialize_time_periods(self, now, user):
-
         days_since_sunday = (now.weekday() + 1) % 7  # Monday is 0, Sunday is 6
         start_of_previous_sunday = now - timedelta(days=days_since_sunday)
         start_of_previous_sunday = start_of_previous_sunday.replace(hour=0, minute=0, second=0, microsecond=0)
         new_week_index = int(start_of_previous_sunday.timestamp())
 
         beginning_of_month = now.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+
         beginning_of_year = now.now().replace(month=1, day=1, hour=0, minute=0, second=0, microsecond=0)
 
-        if not self.time_period_exists(user, "week"):
+        if not time_period_exists(user, "week"):
             # Create an initial time period for the user
             initial_time_period_week = TimePeriod.objects.create(
                 user=user,
@@ -84,7 +106,7 @@ class Command(BaseCommand):
             )
             self.stdout.write(self.style.SUCCESS(f'Created initial time period {initial_time_period_week} for user {user}'))
 
-        if not self.time_period_exists(user, "month"):
+        if not time_period_exists(user, "month"):
             initial_time_period_month = TimePeriod.objects.create(
                 user=user,
                 type='month',  # Default period type
@@ -92,7 +114,7 @@ class Command(BaseCommand):
             )
             self.stdout.write(self.style.SUCCESS(f'Created initial time period {initial_time_period_month} for user {user}'))
 
-        if not self.time_period_exists(user, "year"):
+        if not time_period_exists(user, "year"):
             initial_time_period_year = TimePeriod.objects.create(
                 user=user,
                 type='year',  # Default period type
