de4f20545fd1911359e915b592f2682c0b8f1d53
Melody <melody@mechanist.dev>
2024-06-08 12:07:22 -0500

    Fixed timeperiod roll-over

--- budget/management/commands/update_timeperiods.py
+++ budget/management/commands/update_timeperiods.py
@@ -13,8 +13,18 @@ def time_period_type_exists(user, period_type):
     return TimePeriod.objects.filter(user=user, type=period_type).exists()
 
 
+def current_time_index_exists(user, index):
+    time_p = TimePeriod.objects.filter(user=user, index=index).first()
+    print(time_p)
+    out = CurrentTimePeriod.objects.filter(user=user, period=time_p).exists()
+    print(out)
+    return out
+
+
 def time_period_index_exists(user, index):
-    return TimePeriod.objects.filter(user=user, index=index).exists()
+    out = TimePeriod.objects.filter(user=user, index=index).exists()
+    print(out)
+    return out
 
 
 def copy_categories(user, prev, new):
@@ -30,8 +40,8 @@ def copy_categories(user, prev, new):
 
 def is_in_current_period(prev, fut):
     now = int(timezone.now().timestamp())
-    output = prev <= now <= fut
-    print(f'{prev} < {now} < {fut} = {output}')
+    output = now in range(int(prev), int(fut))
+    print(f'prev {prev} < now {now} < future {fut} = {output}')
     return output
 
 
@@ -83,18 +93,20 @@ class Command(BaseCommand):
                 self.stdout.write(self.style.SUCCESS(f'Created new time period {new_index_year} for user {user}'))
 
     def initialize_time_periods(self, now, user):
+        latest_week = latest_time_period_by_type(user, "week")
         days_since_sunday = (now.weekday() + 1) % 7  # Monday is 0, Sunday is 6
         start_of_previous_sunday = now - timedelta(days=days_since_sunday)
         start_of_previous_sunday = start_of_previous_sunday.replace(hour=0, minute=0, second=0, microsecond=0)
         new_week_index = int(start_of_previous_sunday.timestamp())
 
-        if not time_period_type_exists(user, "week"):
+        if not current_time_index_exists(user, new_week_index):
             # Create an initial time period for the user
             initial_time_period_week = TimePeriod.objects.create(
                 user=user,
                 type='week',  # Default period type
                 index=new_week_index
             )
+            copy_categories(user, latest_week, initial_time_period_week)
 
             curr_time_period = CurrentTimePeriod.objects.create(
                 user=user,
